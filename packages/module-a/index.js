8127ab98144322b97b912534f70c27f70ccc7ced
Jinke Li <a1231236677287@163.com>
2021-11-22 03:16:38 +0000

    fix: module-a fix 11

--- packages/module-a/index.js
+++ packages/module-a/index.js
@@ -3,6 +3,8 @@ module.exports = {
     console.log('module A 1111');
   },
   fix() {
-    console.log('module a bug fix, 并且修改了父级的一些文件111 1.2sdfdsf.7xxx');
+    console.log(
+      'module a bug fix, 并sdfdsdfs且修改了父级的一些文件111 1.2sdfdsf.7xxx',
+    );
   },
 };
