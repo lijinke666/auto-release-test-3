5ff7edf692552526f3af86c28ca5be320bbc9111
Jinke Li <a1231236677287@163.com>
2021-11-24 06:58:59 +0000

    fix: 修复 1.1.3

--- packages/module-a/index.js
+++ packages/module-a/index.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   fix() {
     console.log(
-      'module a bug fix, 并且修改了父级22的一些文件111 sdfs222dfds122.2.71111',
+      'module a bug fix,222 并且修改了父级22的一些文件111 sdfs222dfds122.2.71111',
     );
   },
 };
